3b863fef202b89903cb55a3560750a7760c236b5
Larisa <Larisa@DESKTOP-9JK8JBF>
2024-05-16 21:25:49 +0300

    Lesson 8 Accounting

---
+++ src/main/java/telran/java52/accouting/model/Role.java
@@ -0,0 +1,5 @@
+package telran.java52.accouting.model;
+
+public enum Role {
+USER,MODERATOR,ADMINISTRATOR;
+}
---
+++ src/main/java/telran/java52/accouting/service/UserAccountService.java
@@ -0,0 +1,28 @@
+package telran.java52.accouting.service;
+
+
+
+import telran.java52.accouting.dto.RolesDto;
+import telran.java52.accouting.dto.UserDto;
+import telran.java52.accouting.dto.UserEditDto;
+import telran.java52.accouting.dto.UserRegisterDto;
+import telran.java52.post.service.PostService;
+
+public interface UserAccountService {
+	public static final PostService postService = null;
+	
+	
+	UserDto register(UserRegisterDto userRegisterDto);
+
+	UserDto getUser(String login);
+
+	UserDto removeUser(String login);
+
+	UserDto updateUser(String login, UserEditDto userEditDto);
+
+	RolesDto changeRolesList(String login, String role, boolean isAddRole);
+
+	void changePassword(String login, String newPassword);
+	
+
+}
